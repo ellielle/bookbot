bc8e950f4e335202562c76e351798e0701bc1804
ellielle <40385743+ellielle@users.noreply.github.com>
2023-11-16 13:08:19 -0500

    format

--- main.py
+++ main.py
@@ -5,19 +5,22 @@ def main():
         letters = count_letters(words)
         generate_report(words, letters)
 
+
 def count_words(file_contents):
     return file_contents.split()
+
 
 def count_letters(words):
     letter_dict = {}
     for word in words:
-         for letter in word:
-             if letter.lower() in letter_dict:
-                 letter_dict[letter.lower()] += 1
-             else:
-                 letter_dict[letter.lower()] = 1
-    
+        for letter in word:
+            if letter.lower() in letter_dict:
+                letter_dict[letter.lower()] += 1
+            else:
+                letter_dict[letter.lower()] = 1
+
     return letter_dict
+
 
 def generate_report(words, letters):
     print("--- Begin report of books/frankenstein.txt ---")
@@ -29,12 +32,11 @@ def generate_report(words, letters):
         if k.isalpha():
             letter_list.append([k, v])
 
-    letter_list.sort(key=lambda x:x[1], reverse=True)
+    letter_list.sort(key=lambda x: x[1], reverse=True)
 
     for k, v in letter_list:
-       print(f"The '{k}' character was found {v} times")
+        print(f"The '{k}' character was found {v} times")
     print("--- End report ---")
-
 
 
 main()
